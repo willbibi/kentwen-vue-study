9eedd9386e988e1f0b40b684467a123f2b48bcf7
kent <kentwen@126.com>
2018-03-22 19:42:37 +0800

    fourth
    完成歌手页面

---
+++ src/api/singer.js
@@ -0,0 +1,20 @@
+import {commonParams, options} from 'api/config'
+import jsonp from 'common/js/jsonp'
+
+export function getSingerList() {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/v8.fcg'
+  const data = Object.assign({}, commonParams, {
+    channel: 'singer',
+    page: 'list',
+    key: 'all_all_all',
+    pagesize: 100,
+    pagenum: 1,
+    platform: 'yqq',
+    loginUin: 0,
+    hostUin: 0,
+    needNewCode: 0,
+    g_tk: 5381,
+    format: 'jsonp'
+  })
+  return jsonp(url, data, options)
+}
